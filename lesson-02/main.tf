05c92991ada9b38aa567b95225d7b59ed5e2a2e4
cristianortiz <cristian.ortiznavia82@gmail.com>
2024-04-16 23:24:17 -0400

    [ADD] config tf to use  AWS S3 and dynamoDB as a remote backend

--- lesson-02/main.tf
+++ lesson-02/main.tf
@@ -1,16 +1,16 @@
 terraform {
-    backend "s3" {
-      bucket = "devops-directive-tf-state"
-      key = "tf-infra/terraform.tfstate"
-      region ="us-east-1"
-      dynamodb_table ="terraform-state-locking"
-      encrypt =true
-    }
-    
+  backend "s3" {
+    bucket         = "devops-directive-tf-state"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
 }
 
-required_providers{
-    aws {
+required_providers {
+  aws {
 
-    }
+  }
 }
